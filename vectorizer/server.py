6bbb12f4dd08a0f756451e56ab0b76d384d83a5e
emanjavacas <enrique.manjavacas@gmail.com>
2025-01-10 11:46:46 +0000

    fix vector cache

--- vectorizer/server.py
+++ vectorizer/server.py
@@ -42,19 +42,22 @@ async def vectorize_task(task_id, texts, doc_ids):
             try:
                 app.state.model_manager.load_model()
                 app.state.model_manager.move_model_to_gpu()
-                # cache
-                input_texts = texts
-                if text2vector := (await app.state.db_client.retrieve_cache(texts)):
-                    input_texts = [text for text in texts if text not in text2vector]
-                if input_texts: # we may have retrieved all input texts
+                # Cache
+                if text2vector := await app.state.db_client.retrieve_cache(texts):
+                    if input_texts := [text for text in texts if text not in text2vector]:
+                        vectors = await run_in_threadpool(
+                            app.state.model_manager.get_model().encode, input_texts, settings.BATCH_SIZE)
+                        # merge vectors
+                        text2vector.update(zip(input_texts, vectors.tolist()))
+                    vectors = [text2vector[text] for text in texts]
+                else:
                     vectors = await run_in_threadpool(
-                        app.state.model_manager.get_model().encode, input_texts, settings.BATCH_SIZE)
-                    text2vector.update(zip(input_texts, vectors.tolist()))
-                app.state.model_manager.move_model_to_cpu()
-                # merge vectors
-                vectors = [text2vector[text] for text in texts]
-                # Update the task status to done
+                        app.state.model_manager.get_model().encode, texts, settings.BATCH_SIZE)
+                    vectors = vectors.tolist()
+                app.state.model_manager.move_model_to_cpu()    
+                # store vectors
                 await app.state.db_client.store_vectors(task_id, vectors, doc_ids)
+                # Update the task status to done
                 await app.state.db_client.update_task_status(task_id, Status.DONE)
                 break
             except Exception as e:
